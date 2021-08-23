bdec8c9f224d53a55e5393babba1aa91473021c4
gustavo <gustavo.isaac@gmail.com>
2021-08-23 18:13:16 +0100

    Glide dependencies and viewbinding enabled

---
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/adapters/RestaurantAdapter.kt
@@ -0,0 +1,10 @@
+package com.artemissoftware.tantalusrestaurants.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.artemissoftware.tantalusrestaurants.data.Restaurant
+
+class RestaurantAdapter {
+}
