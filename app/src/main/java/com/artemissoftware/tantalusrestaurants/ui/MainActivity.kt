4c9ccd6180170da861cb734c5e0182652025b13d
gustavo <gustavo.isaac@gmail.com>
2021-08-24 09:55:03 +0100

    MainActivity logic

--- app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/MainActivity.kt
@@ -2,11 +2,37 @@ package com.artemissoftware.tantalusrestaurants.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.artemissoftware.tantalusrestaurants.R
+import com.artemissoftware.tantalusrestaurants.adapters.RestaurantAdapter
+import com.artemissoftware.tantalusrestaurants.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: RestaurantViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val restaurantAdapter = RestaurantAdapter()
+
+
+        binding.apply {
+            recyclerView.apply {
+                adapter = restaurantAdapter
+                layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+
+            viewModel.restaurants.observe(this@MainActivity) { restaurants ->
+                restaurantAdapter.submitList(restaurants)
+            }
+        }
+
     }
 }
