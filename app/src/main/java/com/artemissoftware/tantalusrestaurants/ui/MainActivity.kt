cac3628cb6ac3db6339d419b7a68197d0fe0cb69
gustavo <gustavo.isaac@gmail.com>
2021-08-24 09:41:10 +0100

    lifecycle-livedata dependencie

--- app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.artemissoftware.tantalusrestaurants
+package com.artemissoftware.tantalusrestaurants.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artemissoftware.tantalusrestaurants.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/RestaurantViewModel.kt
@@ -0,0 +1,26 @@
+package com.artemissoftware.tantalusrestaurants.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.artemissoftware.tantalusrestaurants.api.RestaurantApi
+import com.artemissoftware.tantalusrestaurants.data.Restaurant
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RestaurantViewModel @Inject constructor(api: RestaurantApi) : ViewModel() {
+
+
+    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+    val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
+
+
+//    init {
+//        viewModelScope.launch {
+//            val restaurants = api.getRestaurants()
+//            delay(2000)
+//            restaurantsLiveData.value = restaurants
+//        }
+//    }
+}
