f0fbd83aad14540b742fdaad33c1bcb008cc40de
gustavo <gustavo.isaac@gmail.com>
2021-08-24 09:56:18 +0100

    TantalusRestaurantsApplication

---
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/TantalusRestaurantsApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.tantalusrestaurants
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TantalusRestaurantsApplication: Application()
