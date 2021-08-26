0a300c5b8b4f1268a43e6ed7becc60d0cba883bb
gustavo <gustavo.isaac@gmail.com>
2021-08-26 13:57:49 +0100

    RestaurantViewModel update

--- app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/RestaurantViewModel.kt
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/ui/RestaurantViewModel.kt
@@ -1,30 +1,30 @@
 package com.artemissoftware.tantalusrestaurants.ui
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.artemissoftware.tantalusrestaurants.api.RestaurantApi
 import com.artemissoftware.tantalusrestaurants.data.Restaurant
+import com.artemissoftware.tantalusrestaurants.repository.RestaurantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RestaurantViewModel @Inject constructor(api: RestaurantApi) : ViewModel() {
+class RestaurantViewModel @Inject constructor(repository: RestaurantRepository) : ViewModel() {
 
+    private val restaurants = repository.getRestaurants().asLiveData()
 
-    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
-    val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
-
-
-    init {
-
-        viewModelScope.launch {
-            val restaurants = api.getRestaurants()
-            delay(2000) // ONLY FOR TESTING - REMOVE IN REAL APP
-            restaurantsLiveData.value = restaurants
-        }
-    }
+// Old version
+//    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+//    val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
+//
+//
+//    init {
+//
+//        viewModelScope.launch {
+//            val restaurants = api.getRestaurants()
+//            delay(2000) // ONLY FOR TESTING - REMOVE IN REAL APP
+//            restaurantsLiveData.value = restaurants
+//        }
+//    }
 }
