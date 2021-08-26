efd3d870409f5ca73d62fd155e54c998f9a2fbc2
gustavo <gustavo.isaac@gmail.com>
2021-08-26 13:51:12 +0100

    RestaurantRepository

---
+++ app/src/main/java/com/artemissoftware/tantalusrestaurants/repository/RestaurantRepository.kt
@@ -0,0 +1,29 @@
+package com.artemissoftware.tantalusrestaurants.repository
+
+import androidx.room.withTransaction
+import com.artemissoftware.tantalusrestaurants.api.RestaurantApi
+import com.artemissoftware.tantalusrestaurants.data.RestaurantDatabase
+import com.artemissoftware.tantalusrestaurants.util.networkBoundResource
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class RestaurantRepository @Inject constructor(
+    private val api: RestaurantApi,
+    private val db: RestaurantDatabase
+) {
+    private val restaurantDao = db.restaurantDao()
+
+    fun getRestaurants() = networkBoundResource(
+        query = { restaurantDao.getAllRestaurants() },
+        fetch = {
+            delay(2000)
+            api.getRestaurants()
+        },
+        saveFetchResult = { restaurants ->
+            db.withTransaction {
+                restaurantDao.deleteAllRestaurants()
+                restaurantDao.insertRestaurants(restaurants)
+            }
+        }
+    )
+}
